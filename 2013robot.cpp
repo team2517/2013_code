9544370446840c0838666fd89032afa4ddfe5824
Fengchao <ziplineexpress@gmail.com>
2013-02-16 11:52:18 -0800

    v1 of shooter control code

--- 2013robot.cpp
+++ 2013robot.cpp
@@ -8,6 +8,17 @@ class DefaultRobot: public SimpleRobot {
 	CANJaguar jagB;
 	CANJaguar jagC;
 	CANJaguar jagD;
+	CANJaguar shootFront;
+	CANJaguar shootRear;
+	Solenoid lifterA;
+	Solenoid lifterB;
+	Solenoid suctionA;
+	Solenoid suctionB;
+	Solenoid hopperGateA;
+	Solenoid hopperGateB;
+	Solenoid loaderA;
+	Solenoid loaderB;
+	Compressor compressor;
 	double theta;
 	double radius;
 	double phi;
@@ -17,6 +28,7 @@ class DefaultRobot: public SimpleRobot {
 	double outB;
 	double outC;
 	double outD;
+	double shooterSpeed;
 
 public:
 	DefaultRobot(void) :
@@ -26,11 +38,25 @@ public:
 		 * 11 = white = a
 		 * 10 = yellow = b
 		 */
-		joystick(1), jagA(11), //invert
-				jagB(10), //invert
-				jagC(6), jagD(3) 
+		joystick(1), 
+		jagA(11), //invert
+		jagB(10), //invert
+		jagC(6), 
+		jagD(3),
+		shootFront(7),
+		shootRear(2),
+		lifterA(1),
+		lifterB(2),
+		suctionA(3),
+		suctionB(4),
+		loaderA(5),
+		loaderB(6),
+		hopperGateA(7),
+		hopperGateB(8),
+		compressor(1, 1)
 	{
 		Watchdog().SetExpiration(1);
+		compressor.Start();
 	}
 
 	void Autonomous(void) {
@@ -39,11 +65,38 @@ public:
 
 	void OperatorControl(void) {
 		Watchdog().SetEnabled(true);
+		/*
+		 jagA.ChangeControlMode(jagA.kSpeed);
+		 jagA.ConfigEncoderCodesPerRev(360);
+		 jagA.EnableControl();
+		 jagA.ChangeControlMode(jagA.kPercentVbus);
+		 jagA.EnableControl();
+		 Watchdog().Feed();
+		 jagA.ChangeControlMode(jagB.kSpeed);
+		 jagA.ConfigEncoderCodesPerRev(360);
+		 jagA.EnableControl();
+		 jagA.ChangeControlMode(jagB.kPercentVbus);
+		 jagA.EnableControl();
+		 Watchdog().Feed();
+		 jagA.ChangeControlMode(jagC.kSpeed);
+		 jagA.ConfigEncoderCodesPerRev(360);
+		 jagA.EnableControl();
+		 jagA.ChangeControlMode(jagC.kPercentVbus);
+		 jagA.EnableControl();
+		 Watchdog().Feed();
+		 jagA.ChangeControlMode(jagD.kSpeed);
+		 jagA.ConfigEncoderCodesPerRev(360);
+		 jagA.EnableControl();
+		 jagA.ChangeControlMode(jagD.kPercentVbus);
+		 jagA.EnableControl();
+		 Watchdog().Feed();
+		 */
 
 		while (IsOperatorControl()) {
 			phi = joystick.GetRawAxis(3);
 			leftJoyX = joystick.GetRawAxis(1);
 			leftJoyY = -joystick.GetRawAxis(2);
+			radius = sqrt(pow(leftJoyX, 2) + pow(leftJoyY, 2));
 
 			//Left joystick strafe tolerance
 			if ((-.1 < leftJoyX) && (leftJoyX < .1)) {
@@ -53,21 +106,22 @@ public:
 				leftJoyY = 0;
 			}
 
-			radius = sqrt(pow(leftJoyX, 2) + pow(leftJoyY, 2));
-
-			//theta NaN handling
+			//theta NaN handling for X/Y axis movement and calculation
+			//Left stick - X axis
 			if ((leftJoyY == 0) && (leftJoyX != 0)) {
 				if (leftJoyX > 0) {
 					theta = 0;
 				} else if (leftJoyX < 0) {
 					theta = PI;
 				}
+			//Left stick - Y axis
 			} else if ((leftJoyX == 0) && (leftJoyY != 0)) {
 				if (leftJoyY > 0) {
 					theta = PI / 2;
 				} else if (leftJoyY < 0) {
 					theta = (3 * PI) / 2;
 				}
+			//No movement
 			} else if ((leftJoyY == 0) && (leftJoyX == 0)) {
 				theta = 0;
 			} else {
@@ -82,6 +136,7 @@ public:
 				theta += (2 * PI);
 			}
 			
+			//Test drive buttons
 			/*
 			if (joystick.GetRawButton(3)) {
 				theta = 0;
@@ -98,11 +153,13 @@ public:
 			}
 			*/
 
+			//Power equations
 			outA = ((radius) * (sin(theta + (PI / 4))) + phi);
 			outB = ((radius) * (cos(theta + (PI / 4))) + phi);
 			outC = ((radius) * (cos(theta + (PI / 4))) - phi);
 			outD = ((radius) * (sin(theta + (PI / 4))) - phi);
 
+			//Output to motors
 			if (outA > 1) {
 				jagA.Set(-1);
 			} else if (outA < -1) {
@@ -131,17 +188,39 @@ public:
 			} else {
 				jagD.Set(outD);
 			}
+			
+			//Pneumatics
+			if(joystick.GetRawButton(7)){
+				loaderA.Set(false);
+				loaderB.Set(true);
+			}else{
+				loaderA.Set(true);
+				loaderB.Set(false);
+			}
+			
+			
 
-			printf("x: %f y: %f phi: %f\n", leftJoyX, leftJoyY, phi);
-			//("a: %f b: %f c: %f d: %f\n", jagA.Get(), jagB.Get(), jagC.Get(), jagD.Get());
+			//Shooter
+			if(joystick.GetRawButton(1) && shooterSpeed < 1){
+				shooterSpeed += .1;
+			}
+			if(joystick.GetRawButton(2) && shooterSpeed > 0){
+				shooterSpeed -= .1;
+			}
+			if(joystick.GetRawButton(4)){
+				shootFront.Set(shooterSpeed);
+				shootRear.Set(shooterSpeed);
+			}
+			if(joystick.GetRawButton(3)){
+				shootFront.Set(0);
+				shootRear.Set(0);
+			}
+
+			//Diagnostics output
+			//printf("x: %f y: %f phi: %f\n", leftJoyX, leftJoyY, phi);
+			//printf("a: %f b: %f c: %f d: %f\n", jagA.Get(), jagB.Get(), jagC.Get(), jagD.Get());
 			//printf("theta: %f radius: %f\n", theta, radius);
 
-			/*
-			 jagA.Set((radius)*(sin(theta))+phi);
-			 jagB.Set((radius)*(cos(theta))-phi);
-			 jagC.Set((radius)*(cos(theta))+phi);
-			 jagD.Set((radius)*(sin(theta))-phi);
-			 */
 			Watchdog().Feed();
 		}
 	}
